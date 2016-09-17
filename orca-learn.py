793666f24b263c0c7725f22ff3af4e8cef23654e
Jesse D. Williams <jessewilliams@me.com>
2016-09-16 21:34:28 -0700

    Try

--- orca-learn.py
+++ orca-learn.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/post/<string:app_token>', methods=['GET'])
-def show_post(post_id):
+def show_post(app_token):
     # show the post with the given id, the id is an integer
     return 'Token: ' % app_token
