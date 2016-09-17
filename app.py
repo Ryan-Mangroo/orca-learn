e213e298d945390d9b1a872f530932959f224952
Jesse D. Williams <jessewilliams@me.com>
2016-09-16 21:13:51 -0700

    Third

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/post/<string:app_token>', methods=['GET'])
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Token: ' % app_token
