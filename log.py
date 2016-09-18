de76267d5b100087b6b07654c55605e2f5b68cce
Jesse Williams <jess@workwoo.com>
2016-09-18 14:05:58 -0700

    Got a test classify setup working

---
+++ log.py
@@ -0,0 +1,8 @@
+def info(string):
+	print '    INFO: '+string
+
+def function(functionName):
+	print '|' + functionName + '|' 
+
+def error(string):
+	print 'ERROR: '+string
--- orca-learn.py
+++ orca-learn.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import log
+import random
+import os
 app = Flask(__name__)
 
-@app.route('/post/<string:app_token>', methods=['GET'])
-def show_post(app_token):
-    # show the post with the given id, the id is an integer
-    return 'Token: ' % app_token
+@app.route('/classify', methods=['GET'])
+def classify():
+	log.function('classify')
+	
+	token = request.args['token']
+	keywords = request.args['keywords']
+
+	log.info('TOKEN: ' + token)
+	log.info('KEYWORDS: ' + keywords)
+
+	# Check for a null token or keyword
+	if(not token):
+		return jsonify({'message':'No token given'}), 400
+	if(not keywords):
+		return jsonify({'message':'No keywords given'}), 400
+
+
+	# Then, See if the token is valid
+	if(token != os.environ.APP_TOKEN):
+		return jsonify({'message':'Invalid token'}), 401
+
+	# Now we can do the classification
+	results = [
+		{ '1':80, '2':5, '3':5, '4':5, '5':5 },
+		{ '1':5, '2':80, '3':5, '4':5, '5':5 },
+		{ '1':5, '2':5, '3':80, '4':5, '5':5 },
+		{ '1':5, '2':5, '3':5, '4':80, '5':5 },
+		{ '1':5, '2':5, '3':5, '4':5, '5':80 },
+	]
+
+	return jsonify(random.choice(results))
